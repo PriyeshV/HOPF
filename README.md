79fece1223bd018bc922e6d1329a75bae0bb292a
PriyeshV <priyeshvellore@gmail.com>
2018-01-26 20:52:33 +0530

    branch

--- README.md
+++ README.md
@@ -2,9 +2,24 @@
 
 Higher Order Propagation Framework 
 
+The Frameworks provides different kernels for performing Semi-supervised Node classification.
+
 Few Available Kernels: </br>
 1> Graph Convolutional Networks (GCN)  </br>
 2> Node Information Preserving Kernel </br>
 3> Gated GCN </br>
 
 
+view parser.py to
+1> Specify the kernel
+2> Specify the number of hops
+3> Specify the node and neighbor features 
+4> Specify Folds and Percentages to run
+5> Add Skip Connections
+6> Specify Gated GCN Models
+7> Specify Datset, batch size, learning rate, dropout, dimensions
+8> Specify weighted cross entropy loss
+9> Partial Neighborhood
+.. etc
+
+View script_cora.sh and run_cora.py to know how to run multiple kernels in parallel 
--- src/script_cora.sh
+++ src/script_cora.sh
@@ -2,21 +2,37 @@
 #! /bin/sh
 #! /bin/bash
 
-python run_cora.py simple h - 0 5 0 &      # Node
-sleep 2s
-python run_cora.py simple x h 0 5 1 &     # Node#
-sleep 2s
+# Format
+# python run_cora.py simple/kipf_kernels node_features neighbors_features no_outer_epochs GPU_id
+
+# Node Attribute Only Classifier
 python run_cora.py simple h - 0 1 2 &      # Node
 sleep 2s
+
+# Neighbor only Classifier
 python run_cora.py simple - h 0 1 3 # Neighbor
 sleep 2s
+
+# Node Information preserving (NIP) model with unnormalized laplacian kernel
 python run_cora.py simple x h 0 1 4 &   # Simple
 sleep 2s
+
+# Node Information preserving model with Thomas Kipf's GCN style weighted combination
 python run_cora.py kipf x h 0 1 5 &     # Kipf GCN
 sleep 2s
+
+# Thomas Kipf's GCN weights but HOPF version
 python run_cora.py kipf h h 1 1 6 &     # Kipf GCN
 sleep 2s
+
+# Mean kernel
 python run_cora.py simple h h 1 1 7    # Simple
 sleep 2s
 
+# Semi-supervised ICA
+python run_cora.py simple h - 0 5 0 &      # Node
+sleep 2s
+
+# Semi-supervised Iterative NIP Kernels
+python run_cora.py simple x h 0 5 1 &     # Node#
 
